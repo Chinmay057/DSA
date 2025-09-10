705f33aaf205c041726da6ad7095adbccf738952
Chinmay Patil <Chinmayspatil057@gmail.com>
2025-09-10 22:45:34 +0530

    Add files via upload

---
+++ helllooo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1;
+    int number2;
+    cout<<"Enter Your number1:";
+    cin>> number1 ;
+    cout<<"Enter your number2:";
+    cin>> number2;
+    cout << "sum" << number1+number2 << endl;
+    
+
+    return 0;
+}
